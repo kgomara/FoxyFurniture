b059e8068b6b8abfa05667b7eee3ba5bdf1a7820
Kevin O'Mara <kgomara@yahoo.com>
2026-05-05 13:42:54 -0700

    Migrate project to Bootstrap5.x
    Fixed #12.  Fixed #11.  Fixed #10.  Fixed #7. Fixed #5.  Fixed #3.

--- js/ff-photogallery.js
+++
@@ -1,9 +0,0 @@
-/*global $, jQuery, alert, document: false */
-
-$(document).ready(function () {
-	$('ul.first').bsPhotoGallery({
-		"classes" : "col-md-3 col-sm-4 col-xs-6 col-xxs-12",
-		"hasModal" : true
-	});
-});
-
