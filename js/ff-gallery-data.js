33e1cbe856927bfc7fd2e4267f2f494076946eaa
Kevin O'Mara <kgomara@yahoo.com>
2026-09-05 17:34:59 -0700

    fixes #63 Separate gallery data from html

--- js/ff-gallery-data.js
+++ js/ff-gallery-data.js
@@ -18,5 +18,301 @@ window.ffGalleryItems = {
 			src: 'img/gallery/hd/PPVanilla_504x399.jpg',
 			alt: 'Living room with Power Pole'
 		}
+	],
+	crissCross: [
+		{
+			src: 'img/gallery/cc/01_DCP_1795.jpg',
+			alt: 'Criss-Cross showing all parts'
+		},
+		{
+			src: 'img/gallery/cc/BlackPedestal.jpg',
+			alt: 'Black Criss-Cross'
+		},
+		{
+			src: 'img/gallery/cc/CCCherryWithVase_331x500.jpg',
+			alt: 'Cherry Criss-Cross with vase'
+		},
+		{
+			src: 'img/gallery/cc/CCMahoganyCross_331x500.jpg',
+			alt: 'Mahongany Criss-Cross'
+		},
+		{
+			src: 'img/gallery/cc/CCMahoganyShelf_331x500.jpg',
+			alt: 'Mahongany Criss-Cross shelf'
+		},
+		{
+			src: 'img/gallery/cc/DCP_1239.jpg',
+			alt: 'Criss-Cross with tall vase'
+		},
+		{
+			src: 'img/gallery/cc/DCP_1292.jpg',
+			alt: 'Lady Hera waiting for'
+		},
+		{
+			src: 'img/gallery/cc/DCP_1323.jpg',
+			alt: 'Lady Hera at the whipping station'
+		},
+		{
+			src: 'img/gallery/cc/DCP_1909.jpg',
+			alt: 'Criss-Cross as pedestal'
+		}
+	],
+	versaHorse: [
+		{
+			src: 'img/gallery/vh/VHPilloryHeraMain_305x400.jpg',
+			alt: 'Pillory attached to VersahHorse base'
+		},
+		{
+			src: 'img/gallery/vh/000_0004.jpg',
+			alt: 'VersaHorse folded up'
+		},
+		{
+			src: 'img/gallery/vh/000_0009.jpg',
+			alt: 'VersaHorse partially opened'
+		},
+		{
+			src: 'img/gallery/vh/100_1131.jpg',
+			alt: 'VersaHorse fully opened'
+		},
+		{
+			src: 'img/gallery/vh/100_1134.jpg',
+			alt: 'VersaHorse flap-brace locked in place'
+		},
+		{
+			src: 'img/gallery/vh/100_1143.jpg',
+			alt: 'Pillory Bracket - note bolts for attaching to base'
+		},
+		{
+			src: 'img/gallery/vh/100_1148.jpg',
+			alt: 'Pillory bracket bolted to base with wing nuts'
+		},
+		{
+			src: 'img/gallery/vh/100_1152.jpg',
+			alt: 'Stock attached to Pillory Bracket'
+		},
+		{
+			src: 'img/gallery/vh/100_1158.jpg',
+			alt: 'Chest pad for long duration scenes'
+		},
+		{
+			src: 'img/gallery/vh/100_1161.jpg',
+			alt: 'Wings of the Pegasus'
+		},
+		{
+			src: 'img/gallery/vh/100_1164.jpg',
+			alt: 'Wings attach to base'
+		},
+		{
+			src: 'img/gallery/vh/100_1171.jpg',
+			alt: 'Wings held down by back rest'
+		},
+		{
+			src: 'img/gallery/vh/100_1178.jpg',
+			alt: 'Outrigger brackets attach on inside of base'
+		},
+		{
+			src: 'img/gallery/vh/100_1180.jpg',
+			alt: 'Outriggers feed through eyebolts and screw into brackets'
+		},
+		{
+			src: 'img/gallery/vh/100_1183.jpg',
+			alt: 'Pegasus Bench fully assembled'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2768.jpg',
+			alt: 'Stocks in use - standing'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2774.jpg',
+			alt: 'Stocks in use - seated'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2776.jpg',
+			alt: 'Strapped down on the VersaHorse'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2783.jpg',
+			alt: 'On the Pegasus Bench, legs high on outriggers'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2787.jpg',
+			alt: 'Strapped down to Pegasus Bench and blindfolded'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2851.jpg',
+			alt: 'On the Pegasus Bench, legs low on outriggers'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2793.jpg',
+			alt: 'On the Pegasus Bench, available'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2825.jpg',
+			alt: 'Pillory is comfortable for long sessions'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2836.jpg',
+			alt: 'VersaHorse as Pillory'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2843.jpg',
+			alt: 'VersaHorse as simple sawhorse'
+		},
+		{
+			src: 'img/gallery/vh/DCP_2860.jpg',
+			alt: 'VersaHorse with Pegasus Bench'
+		}
+	],
+	pandorasChest: [
+		{
+			src: 'img/gallery/pc/DCP_1898.jpg',
+			alt: "Pandora's Chest, Raised Panel style"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2026.jpg',
+			alt: "Kneeling at side of Pandora's Chest"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2032.jpg',
+			alt: 'Outriggers slide through eyebolts and screw into block'
+		},
+		{
+			src: 'img/gallery/pc/DCP_2055.jpg',
+			alt: "Raised Panel style Pandora's Chest"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2062.jpg',
+			alt: "Mission style Pandora's Chest"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2072.jpg',
+			alt: 'Eyebolts screw into hidden nuts, and the secret drawer stores other pieces'
+		},
+		{
+			src: 'img/gallery/pc/DCP_2076.jpg',
+			alt: "Pandora's Chest with secret drawer open"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2080.jpg',
+			alt: "Pandora's Chest bolsters become kneeling pads"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2092.jpg',
+			alt: "Outriggers can hold legs spread high on Pandora's Chest"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2094.jpg',
+			alt: "Your partner can be comfortable for long-duration play on Pandora's Chest"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2095.jpg',
+			alt: "Arms stretched out and attached to eyebolts at end of Pandora's Chest"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2097.jpg',
+			alt: "Outriggers can hold legs spread high on Pandora's Chest"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2101.jpg',
+			alt: "Outriggers can hold legs spread low on Pandora's Chest"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2103.jpg',
+			alt: "Pandora's Chest can be used as a medical exam play station"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2110.jpg',
+			alt: "Pandora's Chest with crossbar attachment in place"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2114.jpg',
+			alt: "Pandora's Chest bent over the crossbar"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2115.jpg',
+			alt: "Pandora's Chest bent over the crossbar, ready for spanking"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2118.jpg',
+			alt: "Pandora's Chest bent over for spanking"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2280.jpg',
+			alt: "Pandora's Chest Raised Panel style"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2341.jpg',
+			alt: "Pandora's Chest Mission style with rose upholstery"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2350.jpg',
+			alt: "Pandora's Chest with crossbar attached"
+		},
+		{
+			src: 'img/gallery/pc/DCP_2869.jpg',
+			alt: "Pandora's Chest Raised Panel style"
+		},
+		{
+			src: 'img/gallery/pc/PCBentOver_510x500.jpg',
+			alt: "Lady Hera over the Pandora's Chest crossbar"
+		},
+		{
+			src: 'img/gallery/pc/PCMahoganyAnimalPrint_500x330.jpg',
+			alt: "Pandora's Chest Raised Panel style in mahogany with animal-print upholstery"
+		},
+		{
+			src: 'img/gallery/pc/PCVanillaOpen_482x400.jpg',
+			alt: "Pandora's Chest with plenty of storage for linens"
+		}
+	],
+	powerPole: [
+		{
+			src: 'img/gallery/pp/PPVanilla_504x399.jpg',
+			alt: 'PowerPole Hidden in Plain Sight'
+		},
+		{
+			src: 'img/gallery/pp/DCP_2028.jpg',
+			alt: 'PowerPole in use'
+		},
+		{
+			src: 'img/gallery/pp/DCP_2029.jpg',
+			alt: 'PowerPole - Spreader Bars can be set at many heights'
+		},
+		{
+			src: 'img/gallery/pp/DCP_2352.jpg',
+			alt: 'PowerPole with Criss-Cross as pedestal in background'
+		},
+		{
+			src: 'img/gallery/pp/DCP_2353.jpg',
+			alt: 'Getting cropped on the PowerPole'
+		},
+		{
+			src: 'img/gallery/pp/DCP_2354.jpg',
+			alt: 'Lady Hera on the PowerPole butt red from the crop'
+		},
+		{
+			src: 'img/gallery/pp/DCP_2357.jpg',
+			alt: 'Lady Hera whip marks on the PowerPole'
+		},
+		{
+			src: 'img/gallery/pp/DCP_2671.jpg',
+			alt: 'PowerPole - Coffee Table and Coat Rack'
+		},
+		{
+			src: 'img/gallery/pp/PPBondagePost_304x500.jpg',
+			alt: 'PowerPole - The 13 holes fit Spreader Bars at different heights'
+		},
+		{
+			src: 'img/gallery/pp/PPBondagePostHera_292x500.jpg',
+			alt: 'PowerPole - as simple bondage post'
+		},
+		{
+			src: 'img/gallery/pp/PPBondagePostHera_320x500.jpg',
+			alt: 'Playmate tied, gagged and blindfolded to PowerPole'
+		},
+		{
+			src: 'img/gallery/pp/PPWhippingPostKevin_279x500.jpg',
+			alt: 'PowerPole - as classic whipping post'
+		}
 	]
 };
